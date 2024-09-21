6c8276184122b53e5a5a596942aee324e1e54086
anorazhu <zhusm@bc.edu>
2024-09-20 22:44:51 -0400

    Initial Commit

---
+++ WordGarden-SwiftUI/WordGarden_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_SwiftUIApp.swift
+//  WordGarden-SwiftUI
+//
+//  Created by Anora Zhu on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WordGarden_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordGarden-SwiftUITests/WordGarden_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_SwiftUITests.swift
+//  WordGarden-SwiftUITests
+//
+//  Created by Anora Zhu on 9/20/24.
+//
+
+import Testing
+@testable import WordGarden_SwiftUI
+
+struct WordGarden_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
